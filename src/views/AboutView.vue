beb764c4e6d638921ad6f2532babbff370ea969e
Donald Azevedo <dba9514@gmail.com>
2024-08-06 11:49:39 -0400

    Restructuring the home and about pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique rem
+      sit tempore velit cum obcaecati ab quia perspiciatis omnis nemo magnam
+      temporibus suscipit nihil reprehenderit, maxime corrupti commodi. Sint
+      alias ab sequi molestiae excepturi, placeat eius veritatis eveniet
+      perspiciatis deserunt. Explicabo neque repellendus accusantium nobis eaque
+      pariatur maiores debitis ab minima non soluta harum doloribus sint, quo
+      obcaecati nam laboriosam beatae. Porro fuga consequatur cum nulla
+      molestias soluta pariatur incidunt blanditiis quos enim quasi aspernatur
+      dolorum reiciendis, deleniti veniam non, id quod commodi aperiam. Fugiat
+      explicabo magnam ratione esse? Ex, illo ut quo sapiente delectus animi
+      dicta dolor veritatis reprehenderit.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur at vel
+      praesentium dolor voluptatem, delectus distinctio quidem officia maxime
+      sunt?
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur at vel
+      praesentium dolor voluptatem, delectus distinctio quidem officia maxime
+      sunt?
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: {}
+};
 </script>
